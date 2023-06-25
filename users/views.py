85cc8ed2ee4e771660def961d6c51e1accc88c90
jihyunniiii <jita1125@naver.com>
2023-06-25 21:24:04 +0900

    B-028/feat: 마이페이지 팀플 모집 내역 추가

--- users/views.py
+++ users/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import auth
 from django.contrib.auth.models import User
 from main.models import *
+from django.db.models import Prefetch
 
 def mypage(request, id):
     user = get_object_or_404(User, pk=id)
     result = TestResult.objects.first()
+    posts = Post.objects.filter(writer=user)
+    volunteers = []
+    for post in posts:
+        post_volunteers = Volunteer.objects.filter(post=post)
+        volunteers.append({
+            'post': post,
+            'volunteers': post_volunteers
+        })
     context = {
-        'user' : user,
-        'posts' : Post.objects.filter(writer=user),
+        'user': user,
+        'volunteers': volunteers,
         'result': result,
     }
     return render(request, "users/mypage.html", context)
