1527c6d5d87673d5097710affb4e792d48baafe1
pump9918 <hoyeon9918@gmail.com>
2023-06-27 16:05:56 +0900

    B-037/feat: 프로필사진, 태그

--- main/urls.py
+++ main/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('teamtest1/', teamtest1, name="teamtest1"),
     path('result/<int:test_result_id>/', result, name='result'),
     path('<int:id>/volunteer', volunteer, name='volunteer'),
+    path('tag/', tag_list, name="tag_list"),
+    path('tag/<int:tag_id>', tag_posts, name="tag_posts"),
 ]
